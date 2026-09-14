53f230020ace382729010c3c93b6c2f9367b9dd4
Toqa <Toqa@DESKTOP-UPTKCIF>
2026-09-14 14:41:10 +0200

    Add project files.

---
+++ GlobalExceptionPractice/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace GlobalExceptionPractice.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // هنضيف handling هنا بعدين
+            }
+        }
+    }
+}
